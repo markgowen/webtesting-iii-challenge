ed7e17f97d62ec72943400fa29214a718f607a97
Mark Gowen <mark.gowen78@gmail.com>
2019-10-14 14:13:31 -0700

    Updated .spec.js' with unit testing
    controls.spec.js tested toggleLocked = valid
    display.spec.js tested open/unlocked = valid
    display.spec.js tested locked/closed has 'red-led' = valid

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,18 @@
 // Test away!
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+
+import Controls from './Controls';
+
+test('it renders correctly', () => {
+  expect(render(<Controls />)).toMatchSnapshot();
+});
+
+test('toggleLocked is called on lock buttons', () => {
+  const toggleLockedMock = jest.fn();
+  const { getByText } = render(<Controls toggleLocked={toggleLockedMock} closed={true} />);
+
+  fireEvent.click(getByText(/lock gate/i));
+
+  expect(toggleLockedMock).toHaveBeenCalled();
+});
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,30 @@
 // Test away!
+
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+
+import Display from './Display';
+
+test('it renders correctly', () => {
+  expect(render(<Display />)).toMatchSnapshot();
+});
+
+test('displays if gate is open/closed and if it is locked/unlocked', () => {
+  const { getByText } = render(<Display />);
+
+  getByText(/open/i);
+  getByText(/unlocked/i);
+});
+
+test('displays Closed if the closed prop is true and Open if otherwise', () => {
+  const { getByText } = render(<Display closed={false} />);
+//   const label = getByText(/closed/i);
+  getByText((content, element) => element.className.includes('red-led'))
+//   expect(label.className.includes('green-led'));
+});
+
+test('displays Locked if the locked prop is true and Unlocked if otherwise', () => {
+  const { queryByText } = render(<Display locked={true} />);
+  const label = queryByText(/unlocked/i);
+  expect(label).toBeNull();
+});
